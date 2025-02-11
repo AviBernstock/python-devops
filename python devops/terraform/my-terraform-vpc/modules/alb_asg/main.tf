7ffacf49807c2751df72f924a0bb7ed9d7028a53
AviBernstock <103372942+AviBernstock@users.noreply.github.com>
2025-02-11 19:49:45 +0200

    Update main.tf

--- python devops/terraform/my-terraform-vpc/modules/alb_asg/main.tf
+++ python devops/terraform/my-terraform-vpc/modules/alb_asg/main.tf
@@ -89,6 +89,16 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 # Launch Template for EC2 instances
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_launch_template" "web_server_lt" {
   name_prefix   = "web-server-template"
   image_id      = data.aws_ami.ubuntu.id
